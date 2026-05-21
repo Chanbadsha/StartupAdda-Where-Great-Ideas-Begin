d44f978662a935c374faf041ddd511f5fac697c2
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-05-21 07:15:50 +0600

    Implement email password signin

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,15 +2,15 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.NEXT_PUBLIC_MONGODB_URL);
+const client = new MongoClient(process.env.MONGODB_URL);
 const db = client.db();
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: ["http://localhost:3000", "https://startup-adda.vercel.app"],
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
 });
